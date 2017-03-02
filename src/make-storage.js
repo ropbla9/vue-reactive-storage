bb992b130cf78a4d9446e1d5651a24639f8f3687
Rodolfo Paranhos <ropbla9@gmail.com>
2017-03-02 16:58:50 -0300

    removed clean and remove methodsfrom storage / fixed notation

--- src/make-storage.js
+++ src/make-storage.js
@@ -1,30 +1,12 @@
 const store = require('store')
 
-const addActions = storage => Object.assign({
-  remove (key) {
-    if (this[key] === undefined) {
-      console.warn(`${key} not exist in storage`)
-      return
-    }
-
-    this[key] = null
-  },
-  clear () {
-    const blocked = ['clear', 'remove']
-    const keys = Object.keys(this).filter(key => blocked.indexOf(key) === -1)
-    keys.forEach(key => {
-      this[key] = null
-    })
-  }
-}, storage)
-
 module.exports = schema => {
+  // console.log(schema)
   const local = store.getAll()
-  const storage = Object.keys(schema)
-    .reduce((acc, key) => {
-      const value = local[key] || new schema[key]()
-      return Object.assign({ key: value }, acc)
-    }, {})
-
-  return addActions(storage)
+  const storage = Object.keys(schema).reduce((acc, key) => {
+    const value = local[key] || new schema[key]()
+    return Object.assign({ [key]: value }, acc)
+  }, {})
+  // console.log(storage)
+  return storage
 }
